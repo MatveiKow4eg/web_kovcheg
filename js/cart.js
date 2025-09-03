602ece2e3201a32a8923509c8c407772c9bc38d6
MatveiKow4eg <bonantik09@gmail.com>
2025-09-03 16:57:43 +0300

    postcheck

--- js/cart.js
+++ js/cart.js
@@ -152,7 +152,7 @@ function createCartWidget() {
   });
 
   document.getElementById('go-to-cart').addEventListener('click', () => {
-    window.location.href = '/checkout';
+    window.location.href = '/cart';
   });
 
   document.getElementById('continue-shopping').addEventListener('click', () => {
